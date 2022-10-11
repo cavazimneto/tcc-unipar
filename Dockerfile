34cdd939f6be6d6879d57d8cacef34f27a6684b0
cavazimneto <dcavazim@gmail.com>
2022-10-11 03:23:48 -0300

    feat: comentários

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM node
-WORKDIR /app
-COPY package*.json ./
-RUN npm install 
-COPY . .
-EXPOSE 5000
-CMD ["node", "server.js"]
+FROM node                   # Define a imagem base
+WORKDIR /app                # Define o diretório de trabalho
+COPY package*.json ./       # Copia arquivos para destino
+RUN npm install             # Instala o pacote npm
+COPY . .                    # Copia arquivos para destino
+EXPOSE 5000                 # Expoe porta de comunicacao
+CMD ["node", "server.js"]   # Fornece um padrão de execução do Container
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,16 @@
-resource "digitalocean_kubernetes_cluster" "unicluster" {
-  name    = "unicluster"
-  region  = "nyc1"
-  version = "1.24.4-do.0"
 
-  node_pool {
-    name       = "autoscale-worker-pool"
-    size       = "s-2vcpu-2gb"
-    auto_scale = true
-    min_nodes  = 1
-    max_nodes  = 5
+  
+    resource "digitalocean_kubernetes_cluster" "unicluster" {
+      name    = "unicluster"
+      region  = "nyc1"
+      version = "1.24.4-do.0"
+
+
+      node_pool { 
+        name       = "autoscale-worker-pool"
+        size       = "s-2vcpu-2gb"
+        auto_scale = true                      
+        min_nodes  = 1                        
+        max_nodes  = 5                       
   }
-}
-
-terraform {
-  cloud {
-    organization = "dcavazim"
-
-    workspaces {
-      name = "example-workspace"
-    }
-  }
-}
+} 
